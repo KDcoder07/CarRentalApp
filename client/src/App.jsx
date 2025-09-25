dc7db1f614d14491b252085258b3944551122ba7
KDcoder07 <kshitijdeokar02@gmail.com>
2025-09-25 14:01:38 +0530

    Force Home page to load automatically on GitHub Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
-import { Route, Routes, useLocation, Navigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Route, Routes, Navigate, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
-// Pages
 import Home from "./pages/Home";
 import CarDetails from "./pages/CarDetails";
 import Cars from "./pages/Cars";
@@ -10,29 +9,33 @@ import MyBookings from "./pages/MyBookings";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
-
   const location = useLocation();
-  // For HashRouter, remove '/#' if present
-  const currentPath = location.pathname.startsWith('/#')
-      ? location.pathname.slice(2)
-      : location.pathname;
-  const isOwnerPath = currentPath.startsWith('/owner');
+
+  // Redirect to /home on first render if path is empty
+  useEffect(() => {
+    if (location.pathname === "/") {
+      window.location.replace("#/home");
+    }
+  }, [location.pathname]);
+
+  const currentPath = location.pathname.startsWith("/#")
+    ? location.pathname.slice(2)
+    : location.pathname;
+  const isOwnerPath = currentPath.startsWith("/owner");
 
   return (
     <>
       {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
 
       <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/home" element={<Home />} />
-  <Route path="/car-details/:id" element={<CarDetails />} />
-  <Route path="/cars" element={<Cars />} />
-  <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/car-details/:id" element={<CarDetails />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
 
-  {/* Redirect all unknown paths to /home */}
-  <Route path="*" element={<Navigate to="/home" replace />} />
-</Routes>
-
+        {/* Redirect all unknown paths to /home */}
+        <Route path="*" element={<Navigate to="/home" replace />} />
+      </Routes>
     </>
   );
 };
