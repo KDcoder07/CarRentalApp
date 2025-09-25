31ab82910945e8b9a1f491fe14b7468b78359989
Kshitij  Deokar <139894917+KDcoder07@users.noreply.github.com>
2025-09-25 13:33:36 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,35 @@
 import React, { useState } from "react";
-import {Route, Routes, useLocation } from "react-router-dom"; 
+import { Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
-// IMPORT these components
+// Pages
 import Home from "./pages/Home";
 import CarDetails from "./pages/CarDetails";
 import Cars from "./pages/Cars";
 import MyBookings from "./pages/MyBookings";
 
-
 const App = () => {
-
   const [showLogin, setShowLogin] = useState(false);
-  const isOWnerPath = useLocation().pathname.startsWith('/owner');
-  // const location = useLocation();
+
+  const location = useLocation();
+  // For HashRouter, remove '/#' if present
+  const currentPath = location.pathname.startsWith('/#')
+      ? location.pathname.slice(2)
+      : location.pathname;
+  const isOwnerPath = currentPath.startsWith('/owner');
 
   return (
     <>
-      {!isOWnerPath && <Navbar setShowLogin={setShowLogin} />}
-    
-      
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
+
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/car-details/:id" element={<CarDetails/>} />
-        <Route path="/cars" element={<Cars/>} />
-        <Route path="/my-bookings" element={<MyBookings/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/car-details/:id" element={<CarDetails />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
       </Routes>
+    </>
+  );
+};
 
-      </>
-)
-}
-
-export default App
+export default App;
