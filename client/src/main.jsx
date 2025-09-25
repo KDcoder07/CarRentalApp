efa8e8fd98dfd9bb902fdcb0eed443a4f4bec0fa
KDcoder07 <kshitijdeokar02@gmail.com>
2025-09-25 13:53:32 +0530

    Force initial redirect to Home page on GitHub Pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App.jsx';
 import { HashRouter } from 'react-router-dom';
 
+// Force initial redirect to /Home if hash is empty
+if (!window.location.hash) {
+  window.location.hash = '/Home';
+}
+
 createRoot(document.getElementById('root')).render(
   <HashRouter>
     <App />
