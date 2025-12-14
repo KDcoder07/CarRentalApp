b42a1ad626e890802863faba921e3bc4ad845f3a
KDcoder07 <kshitijdeokar02@gmail.com>
2025-12-15 00:27:37 +0530

    feat(layout): add footer and newsletter components, fix homepage layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import CarDetails from "./pages/CarDetails";
 import Cars from "./pages/Cars";
 import MyBookings from "./pages/MyBookings";
+import Footer from "./components/Footer";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -21,9 +22,13 @@ const App = () => {
         <Route path="/cars" element={<Cars />} />
         <Route path="/my-bookings" element={<MyBookings />} />
 
+
         {/* Redirect all unknown paths to /home */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+
+      {!isOwnerPath && <Footer />}
+      
     </>
   );
 };
